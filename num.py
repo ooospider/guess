9e3e97558f19a09f8d5517ecdc354486c7d29029
Aaron Fan <roma94620@gmail.com>
2021-07-24 13:09:29 +0800

    better version

--- num.py
+++ num.py
@@ -1,8 +1,10 @@
 import random
 r = random.randint(1,100)
+count = 0
 while True :
 	number = input ('隨便猜一個數字吧!')
 	number = int (number)
+	count = count+1
 	if number == r :
 		print ('猜對了ㄟ!')
 		break
@@ -10,4 +12,5 @@ while True :
 		print ('太大了!')
 	else :
 		print ('太小了!')
-		
+print ('你猜了',count,'次')
+
